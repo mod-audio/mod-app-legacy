bae7280f102ae7c9d2b6442b98080960ddda21c9
falkTX <falktx@gmail.com>
2015-04-03 17:47:44 +0200

    Finish linux chroot script

--- data/linux/setup-chroot.sh
+++ data/linux/setup-chroot.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 # ------------------------------------------------------------------------------------
+# function to run a command inside the chroot
+
+function run_chroot_cmd() {
+
+echo "
+export HOME=/root
+export LANG=C
+unset LC_TIME
+$@
+" | sudo tee ./chroot/cmd
+
+sudo chroot ./chroot /bin/bash /cmd
+
+}
+
+# ------------------------------------------------------------------------------------
 # stop on error
 
 set -e
@@ -17,14 +33,15 @@ fi
 
 if [ -f ./chroot/chroot-done ]; then
   echo \
-"""
+"
 chroot is already set up.
 Delete $(pwd)/chroot/chroot-done to start over.
-"""
+"
   exit 0
 else
-  sudo rm -rf ./chroot_delete || true
-  sudo mv chroot chroot_delete
+  if [ -x chroot ]; then
+      sudo mv chroot chroot_delete
+  fi
   sudo rm -rf ./chroot_delete || true
 fi
 
@@ -41,6 +58,21 @@ sudo rm -f ./chroot/etc/resolv.conf
 sudo cp /etc/resolv.conf /etc/hosts ./chroot/etc/
 
 # ------------------------------------------------------------------------------------
+# mount basic chroot points
+
+run_chroot_cmd mount -t devpts none /dev/pts
+run_chroot_cmd mount -t proc none /proc
+run_chroot_cmd mount -t sysfs none /sys
+
+# ------------------------------------------------------------------------------------
+# fix upstart
+
+run_chroot_cmd dpkg-divert --local --rename --add /sbin/initctl
+run_chroot_cmd ln -s /bin/true /sbin/initctl
+run_chroot_cmd touch /etc/init.d/systemd-logind
+run_chroot_cmd touch /etc/init.d/modemmanager
+
+# ------------------------------------------------------------------------------------
 # proper lucid repos + backports
 
 echo "
@@ -55,6 +87,40 @@ deb http://security.ubuntu.com/ubuntu lucid-security main restricted universe mu
 
 mkdir -p ./chroot/etc/apt/sources.list.d
 echo "deb http://ppa.launchpad.net/kxstudio-debian/kxstudio/ubuntu lucid main" | sudo tee ./chroot/etc/apt/sources.list.d/kxstudio-repos-tmp.list
+
+# ------------------------------------------------------------------------------------
+# enable kxstudio-repos
+
+run_chroot_cmd apt-get update
+run_chroot_cmd apt-get install kxstudio-repos kxstudio-repos-backports -y --force-yes
+
+# ------------------------------------------------------------------------------------
+# enable toolchain
+
+echo "deb http://ppa.launchpad.net/kxstudio-debian/toolchain/ubuntu lucid main" | sudo tee -a ./chroot/etc/apt/sources.list.d/kxstudio-debian.list
+
+# ------------------------------------------------------------------------------------
+# cleanup
+
+sudo rm ./chroot/etc/apt/sources.list.d/kxstudio-repos-tmp.list
+
+# ------------------------------------------------------------------------------------
+# install updates
+
+run_chroot_cmd apt-get update
+run_chroot_cmd apt-get dist-upgrade -y
+
+# ------------------------------------------------------------------------------------
+# now finally install all needed tools
+
+run_chroot_cmd apt-get install -y autotools-dev build-essential cmake premake git-core subversion
+run_chroot_cmd apt-get install -y libasound2-dev libjack-dev/lucid libjack0/lucid ladspa-sdk lv2-dev
+run_chroot_cmd apt-get install -y libosmesa6-dev libgl1-mesa-dev libglu1-mesa-dev
+run_chroot_cmd apt-get install -y libxinerama-dev libxi-dev libxrender-dev libxcomposite-dev libxcursor-dev
+run_chroot_cmd apt-get install -y libx11-dev libx11-xcb-dev xvfb
+run_chroot_cmd apt-get install -y libcups2-dev libdbus-1-dev
+run_chroot_cmd apt-get install -y libfontconfig1-dev libfreetype6-dev
+run_chroot_cmd apt-get install -y libglib2.0-dev libgtk2.0-dev
 
 # ------------------------------------------------------------------------------------
 # done

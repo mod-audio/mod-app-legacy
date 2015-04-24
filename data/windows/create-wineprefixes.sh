798f4ad9e383c761d5dec2ae81afabf655aac1fd
falkTX <falktx@gmail.com>
2015-04-24 17:07:11 +0200

    Update wineprefixes script JRA-159 #resolve

--- data/windows/create-wineprefixes.sh
+++ data/windows/create-wineprefixes.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
+set -e
+
+if [ ! -f Makefile ]; then
+  cd ../..
+fi
+
 rm -rf ~/.winepy3
 rm -rf ~/.winepy3_x86
 rm -rf ~/.winepy3_x64
+rm -rf data/windows/python
+
+mkdir data/windows/python
 
 export WINEARCH=win32
 export WINEPREFIX=~/.winepy3_x86
 wineboot
-regsvr32 wineasio.dll
 winetricks vcrun2010
 winetricks corefonts
 winetricks fontsmooth=rgb
 
-# cd data/windows/python
-# msiexec /i python-3.4.2.msi /qn
-# wine cx_Freeze-4.3.3.win32-py3.4.exe
-# wine PyQt5-5.3.2-gpl-Py3.4-Qt5.3.1-x32.exe
-# cd ../../..
+cd data/windows/python
+msiexec /i python-3.4.3.msi /qn
+wine cx_Freeze-4.3.4.win32-py3.4.exe
+wine PyQt5-5.4.1-gpl-Py3.4-Qt5.4.1-x32.exe
+cd ../../..
+
+export WINEARCH=win64
+export WINEPREFIX=~/.winepy3_x64
+wineboot
+winetricks vcrun2010
+winetricks corefonts
+winetricks fontsmooth=rgb
+
+cd data/windows/python
+msiexec /i python-3.4.3.amd64.msi /qn
+wine cx_Freeze-4.3.4.win-amd64-py3.4.exe
+wine PyQt5-5.4.1-gpl-Py3.4-Qt5.4.1-x64.exe
+cd ../../..

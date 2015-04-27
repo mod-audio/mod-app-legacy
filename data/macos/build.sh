16febe2b361f4af7a6a8ad269a3f214876c0e58c
falkTX <falktx@gmail.com>
2015-04-27 08:52:07 +0100

    Enable HiRes mode for osx bundle

--- data/macos/build.sh
+++ data/macos/build.sh
@@ -18,6 +18,7 @@ export PYUIC5=/opt/carla/bin/pyuic5
 ##############################################################################################
 # Build Mac App
 
+make clean
 make
 
 rm -rf ./build/
@@ -27,5 +28,10 @@ cp ./source/mod-remote ./source/MOD-Remote.pyw
 # env SCRIPT_NAME=MOD-App    python3 ./data/macos/bundle.py bdist_mac --bundle-name=MOD-App
 env SCRIPT_NAME=MOD-Remote python3 ./data/macos/bundle.py bdist_mac --bundle-name=MOD-Remote
 rm ./source/*.pyw
+
+##############################################################################################
+# Enable HiDPI mode
+
+sed -i -e 's|<string>icon.icns</string>|<string>icon.icns</string>\'$'\n        <key>NSHighResolutionCapable</key> <true/>|' build/MOD-Remote.app/Contents/Info.plist
 
 ##############################################################################################

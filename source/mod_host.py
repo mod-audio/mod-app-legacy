a58f895a850d23b5c534cc9f4dd29454e2ddab0f
falkTX <falktx@gmail.com>
2015-05-13 23:58:37 +0200

    Don't show inspector on Live-ISO

--- source/mod_host.py
+++ source/mod_host.py
@@ -1038,7 +1038,7 @@ class HostWindow(QMainWindow):
             MOD_KEY_WEBVIEW_SHOW_INSPECTOR:   qsettings.value(MOD_KEY_WEBVIEW_SHOW_INSPECTOR,   MOD_DEFAULT_WEBVIEW_SHOW_INSPECTOR,   type=bool)
         }
 
-        inspectorEnabled = self.fSavedSettings[MOD_KEY_WEBVIEW_INSPECTOR]
+        inspectorEnabled = self.fSavedSettings[MOD_KEY_WEBVIEW_INSPECTOR] and not USING_LIVE_ISO
 
         websettings.setAttribute(QWebSettings.DeveloperExtrasEnabled, inspectorEnabled)
 
